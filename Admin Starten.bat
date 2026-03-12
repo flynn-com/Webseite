b57e608d947677d9196eb364fca27e86f7464bd3
Flynn <flynn@example.com>
2026-03-12 21:25:22 +0100

    Verschiebe SaaS Plattform Dateien in separaten Ordner

---
+++ Admin Starten.bat
@@ -0,0 +1,7 @@
+@echo off
+title .FLYNN Admin Center
+echo Starte lokale Desktop App...
+echo Bitte schliesse das Fenster, in dem die App laeuft, wenn du fertig bist.
+echo.
+python admin_app.py
+pause
