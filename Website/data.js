9a79a2f8f585a5bba8af1ba20266d84519ae4be8
Flynn <flynn@example.com>
2026-03-18 23:41:41 +0100

    Website update via Admin App

--- Website/data.js
+++ Website/data.js
@@ -1,80 +1,29 @@
 // Initial Project Data
 const initialProjects = [
     {
-        "id": 1773251099144,
+        "id": 1,
         "number": "01",
-        "title": "Organisch ",
         "bigNumber": "01",
-        "headerDetails": "NUSSKNACKER",
-        "shortDescription": "Der Nussknacker als Designobjekt.",
-        "description": "",
-        "youtube": "",
-        "hasPhoto": false,
-        "hasVideo": false,
-        "hasDesign": true,
-        "companyLogo": null,
-        "gallery": [
-            "assets/projects/img_2d9ae60b.webp",
-            "assets/projects/img_9ecd94b0.webp",
-            "assets/projects/img_de3cacc6.webp",
-            "assets/projects/img_715fe8bc.webp"
-        ],
-        "previewText": "PREVIEW",
-        "mainImageText": "IMG"
-    },
-    {
-        "id": 1773250768865,
-        "number": "02",
         "title": "Ar\u00e9e",
-        "bigNumber": "02",
-        "headerDetails": "M\u00fcnchen",
-        "shortDescription": "R\u00e4ume aus ungesehenen Blickwinkeln",
-        "description": "Die neu gestaltete Immobilie, welche von dem Architekturb\u00fcros Ar\u00e9e eingerichtet wurde, wurde in einer Fotoreihe abgebildet. Dabei lag der Fokus darauf, das Raumkonzept und die Einrichtung durch gezielte Perspektiven in Szene zu setzen.",
-        "youtube": "",
-        "hasPhoto": true,
-        "hasVideo": false,
-        "hasDesign": false,
-        "companyLogo": "assets/projects/img_fccc4fd6.webp",
+        "category": "01",
+        "shortDescription": "R\u00e4ume aus ungesehenen Blickwinkeln ",
+        "description": "Die neu gestaltete Immobilie, welche von dem Architekturb\u00fcros Ar\u00e9e eingerichtet wurde, wurde in einer Fotoreihe abgebildet. Dabei lag der Fokus darauf, das Raumkonzept und die Einrichtung durch gezielte Perspektiven in Szene zu setzen. ",
+        "image": "assets/projects/img_a0ad29d0.jpg",
         "gallery": [
-            "assets/projects/img_7b075ab9.webp",
-            "assets/projects/img_df698a4e.webp",
-            "assets/projects/img_062f41f6.webp",
-            "assets/projects/img_2744ba03.webp",
-            "assets/projects/img_3462a1ec.webp",
-            "assets/projects/img_9d679931.webp",
-            "assets/projects/img_23d52a9f.webp",
-            "assets/projects/img_04f5c653.webp",
-            "assets/projects/img_c3173fc5.webp",
-            "assets/projects/img_fccc5910.webp",
-            "assets/projects/img_e8c8fb22.webp"
+            "assets/projects/img_65dc9081.jpg",
+            "assets/projects/img_e7b532f7.jpg",
+            "assets/projects/img_41b5afaf.jpg",
+            "assets/projects/img_c31411f3.jpg",
+            "assets/projects/img_6da432dd.jpg",
+            "assets/projects/img_f9b1be2a.jpg",
+            "assets/projects/img_414db4af.jpg",
+            "assets/projects/img_ba252d2f.jpg",
+            "assets/projects/img_0c2264d0.jpg",
+            "assets/projects/img_d8aca1bf.jpg"
         ],
-        "previewText": "PREVIEW",
-        "mainImageText": "IMG"
-    },
-    {
-        "id": 1772658855936,
-        "number": "03",
-        "title": "W123 230E",
-        "bigNumber": "03",
-        "headerDetails": "Mercedes",
-        "shortDescription": "Mercedes in seinen 80er Jahren.",
-        "description": "",
-        "youtube": "",
-        "hasPhoto": true,
-        "hasVideo": false,
-        "hasDesign": false,
-        "companyLogo": null,
-        "gallery": [
-            "assets/projects/img_ab9c7775.webp",
-            "assets/projects/img_2da4cb07.webp",
-            "assets/projects/img_d13e58f4.webp",
-            "assets/projects/img_14e9f468.webp",
-            "assets/projects/img_5e37123d.webp",
-            "assets/projects/img_4ac708ec.webp",
-            "assets/projects/img_6e5347ed.webp"
-        ],
-        "previewText": "PREVIEW",
-        "mainImageText": "IMG"
+        "video": "",
+        "icons": [],
+        "companyLogo": ""
     }
 ];
 
